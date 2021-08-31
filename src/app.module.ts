75069859c6c16b81cb22080f80ba7da9eef04d6a
Antoine-Flo <antoine.flouzat@gmail.com>
2021-08-31 16:03:37 +0200

    unknow jwt strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { ChallengesModule } from './challenges/challenges.module';
 import { UsersModule } from './users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Module({
   imports: [
@@ -17,6 +19,9 @@ import { UsersModule } from './users/users.module';
     UsersModule,
   ],
   controllers: [],
-  providers: [AppService],
+  providers: [AppService,   {
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard,
+  },],
 })
 export class AppModule {}
